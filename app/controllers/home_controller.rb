1d47c743b1f429e10a1df16f54443b59f576bc19
Julius Nyule <jlsnyule22@gmail.com>
2017-09-02 15:26:48 +0000

    featured articles added

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def index
+    @posts = Post.where(featured: "true" )
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170819095222) do
+ActiveRecord::Schema.define(version: 20170902105427) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20170819095222) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "user_id"
+    t.boolean  "featured"
   end
 
   create_table "suggestions", force: :cascade do |t|
