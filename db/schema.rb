fb7379ea42f076453265a9b940e5b1547c931617
mwenda-eric <ericmwenda5@gmail.com>
2017-09-12 11:24:43 +0000

    fixed database migration error that was being caused by active admin

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20170907132238) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -48,14 +44,7 @@ ActiveRecord::Schema.define(version: 20170907132238) do
     t.datetime "updated_at",                          null: false
     t.index ["email"], name: "index_admin_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true, using: :btree
-
-  create_table "albums", force: :cascade do |t|
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    
   end
-
 
   create_table "announcements", force: :cascade do |t|
     t.string   "title"
@@ -68,6 +57,19 @@ ActiveRecord::Schema.define(version: 20170907132238) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "ckeditor_assets", force: :cascade do |t|
+    t.string   "data_file_name",               null: false
+    t.string   "data_content_type"
+    t.integer  "data_file_size"
+    t.string   "data_fingerprint"
+    t.string   "type",              limit: 30
+    t.integer  "width"
+    t.integer  "height"
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
+    t.index ["type"], name: "index_ckeditor_assets_on_type", using: :btree
   end
 
   create_table "comments", force: :cascade do |t|
