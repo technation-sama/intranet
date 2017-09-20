d355f28650257ebe367d358e9c051baca544ed76
mwenda-eric <ericmwenda5@gmail.com>
2017-09-20 14:10:56 +0000

    merged admin and user authentication. users can now login to admin panel through google auth only if they have admin rights

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,17 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   after_action :prepare_unobtrusive_flash
 
+  # ActiveAdmin custom authentication method 
+  def authenticate_active_admin_user!
+    authenticate_user!
+    unless current_user.admin?
+      flash[:alert] = "Unauthorized Access!"
+      redirect_to root_path
+    end
+  end
+  
   def after_sign_in_path_for(resource)
     request.env['omniauth.origin'] || stored_location_for(resource) || root_path
   end
+  
 end
