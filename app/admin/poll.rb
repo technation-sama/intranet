80d5d8ac4ad41f7c75f78d4b2ed229549160762b
mwenda-eric <ericmwenda5@gmail.com>
2017-11-17 14:13:07 +0000

    fix polls filter from admin page

--- app/admin/poll.rb
+++ app/admin/poll.rb
@@ -1,33 +1,38 @@
 ActiveAdmin.register Poll do
+  includes :user
+  scope ->{Date.today.strftime("%B") << Date.today.strftime("%Y")}, :current_month, default: true
+  scope :all
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
 # permit_params :list, :of, :attributes, :on, :model
 #
 # or
-#
-current = Date.today.strftime("%B") << Date.today.strftime("%Y")
+# 
+# current = 
+# index do
+# users= Poll.where(period: current).map(&:user_id).uniq
+#   panel "#{current} votes" do 
+#     table_for User.where('id IN (?)', users).order(polls_count: :desc)do
+#       column "Nominee ", :name
+#       column "Number of Votes",:polls_count
+#     end
+#   end
+#   # panel "#{current} Graphical view" do	
+#   #   render 'polls/votercharts'
+#   # end
+# end
 
-
-index do
-users= Poll.where(period: current).map(&:user_id).uniq
-  panel "#{current} votes" do 
-    table_for User.where('id IN (?)', users).order(polls_count: :desc)do
-      column "Nominee ", :name
-      column "Number of Votes",:polls_count
-    end
-  end
-  # panel "#{current} Graphical view" do	
-  #   render 'polls/votercharts'
-  # end
+# actions :all, :except => [:destroy, :update, :edit]
+# permit_params do
+#     params = [:user_id, :body, :project_name]
+#     # params.push :author_id if current_user.admin?
+#     params
+# end
+# filter :period, as: :select
+users = :index
+index as: :block do |user|
+  h2  users
 end
-
-actions :all, :except => [:destroy, :update, :edit]
-permit_params do
-    params = [:user_id, :body, :project_name]
-    # params.push :author_id if current_user.admin?
-    params
-end
-filter :period, as: :select
 config.clear_action_items!
 end
--- app/models/poll.rb
+++ app/models/poll.rb
@@ -1,18 +1,26 @@
 class Poll < ApplicationRecord
   belongs_to :user, counter_cache: true, dependent: :destroy
   validates :user_id, :body, :project_name, presence: true
-
+  @@period= Date.today.strftime("%B") << Date.today.strftime("%Y")
+  
+  def self.ransackable_scopes(_auth_object = nil)
+    [:current_month]
+  end 
+  
   def after_save
    self.update_counter_cache
   end
+  
+  def after_destroy
+    self.update_counter_cache
+  end
 
-def after_destroy
-  self.update_counter_cache
-end
-
-def update_counter_cache
-  period= Date.today.strftime("%B")<<Date.today.strftime("%Y")
-  self.user.polls_count = Poll.count( :conditions => ["period = period AND user_id = ?",self.user.id])
-  self.user.save
-end
+  def update_counter_cache
+    self.user.polls_count = Poll.count( :conditions => ["period = @@period AND user_id = ?",self.user.id])
+    self.user.save
+  end
+  
+  scope :current_month, -> {
+    where(period: @@period)
+  }
 end
