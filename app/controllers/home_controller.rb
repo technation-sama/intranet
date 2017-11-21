b553f2eed0173e9ceb730cd12b02f0eafeb1a75c
mwenda-eric <ericmwenda5@gmail.com>
2017-11-21 13:36:36 +0000

    Home page Announcement display

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,7 @@ class HomeController < ApplicationController
   def index
     @posts = Post.where(featured: "true", published: "true" ).page(params[:page]).per(5)
     @events = Event.all
+    
+    @announcements=Announcement.order('id DESC').take(8)
   end
 end
