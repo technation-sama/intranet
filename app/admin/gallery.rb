e2a23d6ba4ec5c222afba2ee0c70cecd4e92f11e
victorjambo <victorjambo@live.com>
2017-11-24 00:45:01 +0300

    [Chore] buttons housekeeping

--- app/admin/gallery.rb
+++ app/admin/gallery.rb
@@ -11,9 +11,8 @@ ActiveAdmin.register Gallery do
   scope :all, default: true
   scope :empty
   permit_params :title, gallery_attachments_attributes: [:id, :gallery_id, :photo]
-  
   filter :title, as: :select
-
+  
   index do
     index_column
     column :title do |gallery|
@@ -26,8 +25,11 @@ ActiveAdmin.register Gallery do
       slug.gallery_attachments.size
     end
 
-    actions do |gallery|
-      a "View in page", href: gallery_gallery_attachments_path(gallery), class: "member_link"
+    actions defaults: false do |gallery|
+      item 'Edit Title', edit_admin_gallery_path(gallery), class: 'btn-warning member_link'
+      item 'View Gallery', admin_gallery_gallery_attachments_path(gallery), class: 'member_link'
+      item 'View on site', gallery_gallery_attachments_path(gallery), class: 'member_link'
+      item 'Delete Gallery', admin_gallery_path(gallery), class: 'btn-danger member_link', method: :delete
     end
   end
   
@@ -53,11 +55,11 @@ ActiveAdmin.register Gallery do
   end
   
   form do |f|
-    f.inputs 'Details' do
+    f.inputs 'Gallery Title' do
       f.input :title
     end
   
-    f.inputs do
+    f.inputs 'Gallery Images' do
       f.has_many :gallery_attachments, new_record: 'Upload Images' do |pic|
         pic.input :photo, input_html: { name: "gallery_attachments[photo][]", multiple: true }
       end
@@ -70,15 +72,6 @@ end
 
 
 ### ToDo
-## Gallery attachments
-# show page of image
-# image should link to show page
-# view button should read 'view on page' and lead to page
-# remove edit button
-# rename side menu of GA to 'All Photos'
 ## Gallery
+# Change form for edit
 # work on empty
-# view should lead to /gallery_attachments
-# Edit should be 'edit title'
-# delete 'Delete entire gallery'
-# add photo to this gallery
--- app/admin/gallery_attachment.rb
+++ app/admin/gallery_attachment.rb
@@ -1,9 +1,25 @@
 ActiveAdmin.register GalleryAttachment do
   belongs_to :gallery, optional: true
+  before_filter :skip_sidebar!
+  menu false
+
   index do
     column :photo do |pic|
-      image_tag pic.photo.thumb.url, class: "admin-thumb-image"
+      image_tag pic.photo, class: "admin-thumb-image"
     end
     actions
   end
+
+  action_item only: :index do
+    link_to "Add Photo to Gallery", new_admin_gallery_gallery_attachment_path
+	end
+
 end
+
+### ToDo
+## Gallery attachments
+# image should link to show page
+# view button should read 'view on page' and lead to page
+# remove edit button
+# remove 'new gallery att*'
+# controller for delete, edit
