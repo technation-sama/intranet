80be198218c2ee75b6e59ed5d4e71dab2725579b
mwenda-eric <ericmwenda5@gmail.com>
2017-12-20 09:53:24 +0000

    production

--- config/puma.rb
+++ config/puma.rb
@@ -11,6 +11,7 @@ shared_dir = "#{app_dir}/shared"
 rails_env = ENV['RAILS_ENV'] || "production"
 environment rails_env
 
+#
 # Set up socket location
 bind "unix://#{shared_dir}/sockets/puma.sock"
 
