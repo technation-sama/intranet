d41db367c251089aba9e2b122ab2cdaf4af8aca9
mwenda-eric <ericmwenda5@gmail.com>
2018-01-23 13:30:44 +0000

    fix multiple mails sending as one

--- test/mailers/previews/poll_mailer_preview.rb
+++ test/mailers/previews/poll_mailer_preview.rb
@@ -1,4 +1,7 @@
 # Preview all emails at http://localhost:3000/rails/mailers/poll_mailer
 class PollMailerPreview < ActionMailer::Preview
-
+  def open_poll_email
+    poll_month = 1.month.ago.strftime("%B") << " "<< 1.month.ago.strftime("%Y")
+    PollMailer.open_poll_email(poll_month)
+  end
 end
