a02b2d68e606064a0ad086d818ced2efa8f9af03
Victor <victor@victor-mint.samadc.corp>
2019-10-14 04:37:02 +0300

    field validation

--- app/controllers/repair_controller.rb
+++ app/controllers/repair_controller.rb
@@ -1,5 +1,9 @@
 class RepairController < ApplicationController
-
+  validates :user_id, presence: true
+  validates :category, presence: true
+  validates :description, presence: true
+  validates :email, presence: true
+  
   def index
     @repairs = MaintainaceReport.all
   end
--- app/models/maintainace_report.rb
+++ app/models/maintainace_report.rb
@@ -1,4 +1,8 @@
 class MaintainaceReport < ApplicationRecord
+  validates :user_id, presence: true
+  validates :category, presence: true
+  validates :description, presence: true
+  validates :email, presence: true
   
 
 end
