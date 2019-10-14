1b531a2641017f8d2235b002cccd0705edfa8586
Victor <victor@victor-mint.samadc.corp>
2019-10-14 05:44:18 +0300

    add validation to table

--- app/models/maintainace_report.rb
+++ app/models/maintainace_report.rb
@@ -1,5 +1,7 @@
 class MaintainaceReport < ApplicationRecord
-  
-
+  validates :user_id, presence: true
+  validates :location, presence: true
+  validates :category, presence: true
+  validates :description, presence: true
 
 end
