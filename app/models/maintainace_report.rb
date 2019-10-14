9f4a7c8a95ab697b82f7c1ec490a73589bba925d
Victor <victor@victor-mint.samadc.corp>
2019-10-14 04:42:03 +0300

    field validation2

--- app/models/maintainace_report.rb
+++ app/models/maintainace_report.rb
@@ -1,8 +1,5 @@
 class MaintainaceReport < ApplicationRecord
-  validates :user_id, presence: true
-  validates :category, presence: true
-  validates :description, presence: true
-  validates :email, presence: true
   
+
 
 end
