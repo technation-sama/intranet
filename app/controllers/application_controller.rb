08a2e198b20e282992901a03be4b8c617fbe86c2
Victor <victor@victor-mint.samadc.corp>
2019-11-20 14:01:25 +0300

    ip filter - restore all filter code and add path to module

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   after_action :prepare_unobtrusive_flash
   helper_method :notices
-  
+  config.autoload_paths += %W(#{config.root}/lib) 
+
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  #ActiveAdmin custom authentication method 
+  #ActiveAdmin custom authentication method
   def authenticate_active_admin_user!
       authenticate_user!
     unless current_user.admin?
@@ -13,22 +14,22 @@ class ApplicationController < ActionController::Base
       redirect_to root_path
     end
   end
-  
+
   def after_sign_in_path_for(resource)
     request.env['omniauth.origin'] || stored_location_for(resource) || root_path
   end
-  
+
   def access_denied(exception)
     redirect_to admin_organizations_path, alert: exception.message
   end
-  
+
 protected
   def configure_permitted_parameters
     # Permit the `role and admin` parameter along with the other
     # sign up parameters.
     devise_parameter_sanitizer.permit(:account_update, keys: [:role, :admin])
   end
-  
+
 private
   def notices
     @notices=Notice.all
