d6d4420323d27f42c8d5ba99376e20985c87a270
Victor <victor@victor-mint.samadc.corp>
2019-11-22 18:17:19 +0300

    ip filter restriction - controller specific filter - test 1 link

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -8,7 +8,7 @@ class CategoriesController < ApplicationController
     def samafilter
         user_ip = IPAddr.new(request.remote_ip)
         # An array of IPs and IP ranges that should be allowed. Stored on the current user.
-        allowed_ips = ENV["LINKS_SAMA"].split(",")
+        allowed_ips = "41.215.29.158/30"
         # Validate IP only if allowed_ips array is set, otherwise there is no IP restriction
         if allowed_ips
             verified = false
