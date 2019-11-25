8fbefed5bfc6dd8e5665cbabf3bc17d20952962c
Victor <victor@victor-mint.samadc.corp>
2019-11-25 08:46:56 +0300

    ip filter restriction - controller specific filter - log ip output

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -8,20 +8,22 @@ class CategoriesController < ApplicationController
     def samafilter
         user_ip = IPAddr.new(request.remote_ip)
         # An array of IPs and IP ranges that should be allowed. Stored on the current user.
-        allowed_ips = ENV["LINKS_SAMA"]
+        allowed_ips = ENV["LINKS_SAMA"].inspect
         # Validate IP only if allowed_ips array is set, otherwise there is no IP restriction
-        if allowed_ips
-            verified = false
-            allowed_ips.each do |allowed_ip|
-                allowed_ip = IPAddr.new(allowed_ip)
-                if allowed_ip.include?(user_ip)
-                    verified = true
-                end
-            end
-            # Redirect back to main page if not verified
-            unless verified
-                redirect_to root_url
-            end
+        if false
+          if allowed_ips
+              verified = false
+              allowed_ips.each do |allowed_ip|
+                  allowed_ip = IPAddr.new(allowed_ip)
+                  if allowed_ip.include?(user_ip)
+                      verified = true
+                  end
+              end
+              # Redirect back to main page if not verified
+              unless verified
+                  redirect_to root_url
+              end
+          end
         end
     end
 end
